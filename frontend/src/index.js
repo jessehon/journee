dc16898595842b34a49f144a6c694acd8d2f4d47
Andrej Griniuk <andrej.griniuk@gmail.com>
2018-08-04 20:42:03 +1000

    styling

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,6 +6,17 @@ import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 
 const theme = createMuiTheme({
     palette: {
+        primary: {
+            main: 'rgba(0, 200, 83, 0.85)',
+            light: '#00c853',
+        },
+        secondary: {
+            main: '#311b92'
+        },
+        text: {
+            primary: 'rgba(0, 0, 0, 0.75)'
+        },
+        // These properties below do nothing:
         primary1Color: "rgba(0, 200, 83, 0.85)",
         primary2Color: "#00c853",
         primary3Color: "#9e9e9e",
